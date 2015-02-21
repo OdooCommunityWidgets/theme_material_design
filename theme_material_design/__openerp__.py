92f3b8d7051dd3494240f82fea6034afb9d846cf
Luke Branch <lukebranch@gmail.com>
2015-02-21 13:35:30 +0800

    Create __openerp__.py

---
+++ theme_material_design/__openerp__.py
@@ -0,0 +1,22 @@
+{
+    'name': 'Material Design Theme',
+    'description': 'Material Design Theme',
+    'image': 'material_design_description.jpg',
+    'category': 'Theme',
+    'version': '1.0',
+    'author': 'Luke Branch',
+    'depends': ['theme_common'],
+    'data': [
+        'views/assets.xml',
+        'views/customize_modal.xml',
+        'views/image_content.xml',
+        'views/image_library.xml',
+        'views/snippets_options.xml',
+        'views/snippets.xml',
+    ],
+    'demo': [
+        'demo/home.xml',
+        'demo/blocks.xml',
+    ],
+    'application': True,
+}
